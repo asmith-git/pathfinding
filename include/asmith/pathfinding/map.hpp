fecc5a30493daaa123b83c96e8c8d74e9ea45c6f
Adam Smith <adam.george.smith@btinternet.com>
2017-06-03 10:08:47 +0100

    Path map definition

---
+++ include/asmith/pathfinding/map.hpp
@@ -0,0 +1,38 @@
+//	Copyright 2017 Adam Smith
+//	Licensed under the Apache License, Version 2.0 (the "License");
+//	you may not use this file except in compliance with the License.
+//	You may obtain a copy of the License at
+// 
+//	http://www.apache.org/licenses/LICENSE-2.0
+//
+//	Unless required by applicable law or agreed to in writing, software
+//	distributed under the License is distributed on an "AS IS" BASIS,
+//	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//	See the License for the specific language governing permissions and
+//	limitations under the License.
+
+#ifndef ASMITH_PATHFINDING_MAP_HPP
+#define ASMITH_PATHFINDING_MAP_HPP
+
+#include <cstdint>
+	
+namespace asmith { namespace pathfinding {
+	
+	
+	template<class K = uint64_t, class C = float>
+	class map {
+	public:
+		typedef K key_t;
+		typedef C cost_t;
+		typedef std::pair<key_t, cost_t> connection_t;
+		
+		virtual ~map() {
+			
+		}
+		
+		virtual bool is_connected(key_t, key_t, cost_t&) const throw() = 0;
+		virtual const connection_t* get_connections(key_t, size_t&) const throw() = 0;
+	};
+}}
+
+#endif
